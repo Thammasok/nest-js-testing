65cfbe12659e6fd494dc8ee24682f29027c455b5
Jaranchai Nut <jaranchai.nt@hotmail.com>
2023-07-29 13:27:46 +0700

    testing

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { MathController } from './modules/math/math.controller';
-import { MathService } from './modules/math/math.service';
-import { MathModule } from './modules/math/math.module';
+import { RestaurantsModule } from './restaurants/restaurants.module';
+
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
+import { MealModule } from './meal/meal.module';
 
 @Module({
-  imports: [MathModule],
-  controllers: [AppController, MathController],
-  providers: [AppService, MathService],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.env.${process.env.NODE_ENV}`,
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.DB_URI),
+    RestaurantsModule,
+    AuthModule,
+    MealModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
